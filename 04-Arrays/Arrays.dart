36df6e6d2fc7877a8a76232f3428736fc67d9b6b
Bene-Tesla <benevaniosantos930@gmail.com>
2023-09-10 18:39:18 -0300

    :memo: Usando Arrays em Dart

---
+++ 04-Arrays/Arrays.dart
@@ -0,0 +1,8 @@
+void main(){
+  var textos =["Benevanio", "Santos"];
+  var numeros =[1, 2.9, 6, 69];
+  var condicoes =[!false, true, false, null];
+  print('Eu sou o priemeiro elemento do Array: ${textos[0]}');
+  print("EU sou o segundo elemento do array de numeros: ${numeros[1]}");
+  print("Eu sou o valor false do array de condiçoes: ${condicoes[2]}");
+}
