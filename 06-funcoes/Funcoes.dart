773fcd52c33f9f17e3fc2423c2bc442280232fc0
Bene-Tesla <benevaniosantos930@gmail.com>
2023-09-13 10:03:39 -0300

    :sparkles: introduzindo o conceito de funcoes no dart

---
+++ 06-funcoes/Funcoes.dart
@@ -0,0 +1,16 @@
+void semRetorno() {
+  print("06.0 Função sem Retorno");
+  void conceito() {
+    print("Função void sem retorno");
+  }
+  conceito();
+  void somar(int valorA , int valorB){
+    var result = valorA + valorB;
+    print("O valor do resultado é: $result");
+  }
+  somar(10,39);
+}
+
+void main() {
+  semRetorno();
+}
