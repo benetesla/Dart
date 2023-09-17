885b41cdddb458201c3cc578108edb893519f867
Bene-Tesla <benevaniosantos930@gmail.com>
2023-09-16 21:47:22 -0300

    :art: usando o map e o queue como conceito de coleçoes no dart

---
+++ 16-colecoes/3-Queue/Queue.dart
@@ -0,0 +1,9 @@
+import 'dart:collection';
+
+queue(){
+  Queue <int> queue = Queue();
+  print("implementação: ${queue.runtimeType}");
+}
+void main(){
+  queue();
+}
---
+++ 16-colecoes/4-map/Map.dart
@@ -0,0 +1,10 @@
+  linkedHashmap(){
+    List<String> nomes = ["Benevanio", "Beniel"];
+    Map<int, dynamic> nomesMap = nomes.asMap();
+    print("\n$nomesMap");
+    
+  }
+
+void main(){
+  linkedHashmap();
+}
